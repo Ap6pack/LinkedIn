72300575e9d73af47f208199a6ec688e8962d3ca
Adam Rhys Heaton <adamslinuxemail@gmail.com>
2018-12-28 05:12:06 -0500

    Created first page

---
+++ Introduction to Web Design and Development/js/index.js
@@ -0,0 +1,13 @@
+document.getElementById('color').onclick = changeColor;
+var currentColor = "red";
+
+function changeColor() {
+    if (currentColor == "red") {
+        document.body.style.color = "green";
+        currentColor = "green";
+    } else {
+        document.body.style.color = "red";
+        currentColor = "red";
+    }
+    return currentColor;
+}
