be150210bf6e39cd2fa4d4c28df629281ca41b14
Adam Rhys Heaton <adamslinuxemail@gmail.com>
2019-01-15 01:23:47 -0500

    corrected spelling

--- LearningBashScripting/my.sh
+++ LearningBashScripting/my.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # This is a basic script
 
-a=Hello # a veriable
+a=Hello # a variable
 b="Good Morning" # a string
-c=16 # a interiger
+c=16 # a integer
 d=$(pwd) # the working directory
 e=$(ping -c 1 example.com | grep "bytes from" | cut -d = -f 4) # determines how long a ping request takes to get a response from a server
 
-echo $a # prints a veriable
+echo $a # prints a variable
 echo $b # prints a string
-echo $c # prints an interiger
+echo $c # prints an integer
 echo $d # prints the working directory
 
 echo "$b! I have $c apples." # prints a string with variables inclued
