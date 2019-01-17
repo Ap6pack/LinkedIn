bf42b3747367ca38c638f66907795c5e01f858d6
Adam Rhys Heaton <adamslinuxemail@gmail.com>
2019-01-17 04:22:33 -0500

    added three argument examples using flags

--- LearningBashScripting/arg.sh
+++ LearningBashScripting/arg.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
 
+# while loop that takes user defined using flags
+
+while getopts u:p: option; do
+    case $option in
+        u) user=$OPTARG;; # user flag
+        p) pass=$OPTARG;; # password flag
+    esac
+done
+
+echo "User: $user / Pass: $pass"
+
+while getopts u:p:ab option; do # ab with out colons are check flags
+    case $option in
+        u) user=$OPTARG;; # user flag
+        p) pass=$OPTARG;; # password flag
+        a) echo "Got the A flag";;
+        b) echo "Got the B flag";;
+    esac
+done
+
+echo "User: $user / Pass: $pass"
+
+while getopts :u:p:ab option; do # a leading colons are check flags that have not be used
+    case $option in
+        u) user=$OPTARG;; # user flag
+        p) pass=$OPTARG;; # password flag
+        a) echo "Got the A flag";;
+        b) echo "Got the B flag";;
+        ?) echo "I do not know what $OPTARG is!";;
+    esac
+done
+
+echo "User: $user / Pass: $pass"
